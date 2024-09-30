0cf4c8b7ac72a94235e535bd1f8838936100189d
SRT-Online <101435248+SRT-Online@users.noreply.github.com>
2024-09-30 17:43:25 +0100

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,17 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.HTMLButton")
+                .setHost(host,"xui_ui_htmlbutton3")
+                .setLeft("40.38095238095238em")
+                .setTop("38.095238095238095em")
+                .setWidth("20.571428571428573em")
+                .setHeight("7.161904761904762em")
+                .setCaption("Native Button")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
